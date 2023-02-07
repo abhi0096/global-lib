c01d51afe6759d9d2109505c70dc2910879e3cc5
Abhishek Khandelwal <106994013+abhi0096@users.noreply.github.com>
2023-02-08 01:35:59 +0530

    Update genericFile.groovy

--- vars/genericFile.groovy
+++ vars/genericFile.groovy
@@ -10,7 +10,8 @@ stages {
     stage('git clone') {
        steps {
           // git url: '${pipelineParams.url}', branch: '${pipelineParams.branch}'
-           git branch: '${pipelineParams.branch}', credentialsId: 'Jenkins-git-cred-new', url: '${pipelineParams.url}'
+           //git branch: '${pipelineParams.branch}', credentialsId: 'Jenkins-git-cred-new', url: '${pipelineParams.url}'
+           git branch: '${pipelineParams.branch}', credentialsId: 'Jenkins-git-cred-new', url: 'https://github.com/abhi0096/demo-webpage.git'
         }
     }
     stage('Build the code'){
