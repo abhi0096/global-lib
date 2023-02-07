6cec81f58f209852d4518feede59e60e3dc95e0d
Abhishek Khandelwal <106994013+abhi0096@users.noreply.github.com>
2023-02-07 19:49:19 +0530

    Update genericFile.groovy

--- vars/genericFile.groovy
+++ vars/genericFile.groovy
@@ -1,1 +1,22 @@
+def call(Map pipelineParams = [:]) {
 
+pipeline {
+     agent any
+    //  options {
+    //     copyArtifactPermission('rollback');
+    // }
+
+stages {
+    stage('git clone') {
+       steps {
+           git branch: ${pipelineParams.branch}, url: ${pipelineParams.url}
+        }
+    }
+    stage('Build the code'){
+        steps{
+            sh 'maven clean install'
+        }
+    }
+}
+}
+}
