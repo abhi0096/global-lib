7bbe14a67a8396126bf0a21470d7719b81220578
Abhishek Khandelwal <106994013+abhi0096@users.noreply.github.com>
2023-02-08 20:26:52 +0530

    Update genericFile.groovy

--- vars/genericFile.groovy
+++ vars/genericFile.groovy
@@ -19,7 +19,7 @@ def call(String repoUrl) {
            }
            stage("Checkout Code") {
                steps {
-                   git branch: 'main',
+                   git branch: 'master',
                        url: "${repoUrl}"
                }
            }
