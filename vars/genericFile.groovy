15af1010b8e2599a9f1f442ae4260bd5b2debba1
Abhishek Khandelwal <106994013+abhi0096@users.noreply.github.com>
2023-02-10 00:43:31 +0530

    Update genericFile.groovy

--- vars/genericFile.groovy
+++ vars/genericFile.groovy
@@ -1,68 +1,92 @@
-vars
-| --- welcomeJob.groovy
-| --- jenkinsForJava.groovy
+#!/usr/bin/env groovy
 
-// jenkinsForJava.groovy
-def call(String repoUrl) {
-  pipeline {
-       agent any
-       tools {
-           maven 'Maven 3.5.0'
-           jdk 'jdk8'
-       }
-       stages {
-           stage("Tools initialization") {
-               steps {
-                   sh "mvn --version"
-                   sh "java -version"
-               }
-           }
-           stage("Checkout Code") {
-               steps {
-                   git branch: 'master',
-                       url: "${repoUrl}"
-               }
-           }
-           stage("Cleaning workspace") {
-               steps {
-                   sh "mvn clean"
-               }
-           }
-           stage("Running Testcase") {
-              steps {
-                   sh "mvn test"
-               }
-           }
-           stage("Packing Application") {
-               steps {
-                   sh "mvn package -DskipTests"
-               }
-           }
-       }
-   }
+def call(Map config) {
+    pipeline{
+        agent any
+        stages{
+            stage('git-clone'){
+                steps{
+                    def checkoutStep = {
+                        git url: "${config.repoUrl}", branch: "${config.branch}", credentialsId: "${config.credentialsId}"
+                }
+            }
+        }
+            stage('build'){
+                steps{
+                    def buildStep = {
+                          sh "maven clean install"
+                    }
+                }
+            }
+        }
+    }
 }
 
-// def call(Map config) {
+// vars
+// | --- welcomeJob.groovy
+// | --- jenkinsForJava.groovy
 
-// pipeline {
-//      agent any
-//     //  options {
-//     //     copyArtifactPermission('rollback');
-//     // }
+// // jenkinsForJava.groovy
+// def call(String repoUrl) {
+//   pipeline {
+//        agent any
+//        tools {
+//            maven 'Maven 3.5.0'
+//            jdk 'jdk8'
+//        }
+//        stages {
+//            stage("Tools initialization") {
+//                steps {
+//                    sh "mvn --version"
+//                    sh "java -version"
+//                }
+//            }
+//            stage("Checkout Code") {
+//                steps {
+//                    git branch: 'master',
+//                        url: "${repoUrl}"
+//                }
+//            }
+//            stage("Cleaning workspace") {
+//                steps {
+//                    sh "mvn clean"
+//                }
+//            }
+//            stage("Running Testcase") {
+//               steps {
+//                    sh "mvn test"
+//                }
+//            }
+//            stage("Packing Application") {
+//                steps {
+//                    sh "mvn package -DskipTests"
+//                }
+//            }
+//        }
+//    }
+// }
 
-// stages {
-//     stage('git clone') {
-//        steps {
-//           // git url: '${pipelineParams.url}', branch: '${pipelineParams.branch}'
-//            //git branch: '${pipelineParams.branch}', credentialsId: 'Jenkins-git-cred-new', url: '${pipelineParams.url}'
-//             git branch: '${config.branch}', credentialsId: 'Jenkins-git-cred-new', url: '${config.url}'
-//         }
-//     }
-//     stage('Build the code'){
-//         steps{
-//             sh 'maven clean install'
-//         }
-//     }
-// }
-// }
-// }
+// // def call(Map config) {
+
+// // pipeline {
+// //      agent any
+// //     //  options {
+// //     //     copyArtifactPermission('rollback');
+// //     // }
+
+// // stages {
+// //     stage('git clone') {
+// //        steps {
+// //           // git url: '${pipelineParams.url}', branch: '${pipelineParams.branch}'
+// //            //git branch: '${pipelineParams.branch}', credentialsId: 'Jenkins-git-cred-new', url: '${pipelineParams.url}'
+// //             git branch: '${config.branch}', credentialsId: 'Jenkins-git-cred-new', url: '${config.url}'
+// //         }
+// //     }
+// //     stage('Build the code'){
+// //         steps{
+// //             sh 'maven clean install'
+// //         }
+// //     }
+// // }
+// // }
+// // }
