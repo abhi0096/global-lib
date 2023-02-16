ac2cac8aa412656bd8cb35a89eb51b7d72af49ed
Abhishek Khandelwal <106994013+abhi0096@users.noreply.github.com>
2023-02-16 11:27:36 +0530

    Update genericFile.groovy

--- vars/genericFile.groovy
+++ vars/genericFile.groovy
@@ -1,23 +1,14 @@
 #!/usr/bin/env groovy
 
 def call(Map config) {
-
 pipeline {
      agent any
-    //  options {
-    //     copyArtifactPermission('rollback');
-    // }
-
 stages {
     stage('git-clone') {
        steps {
-            //def step = {
               git url: "${config.repoUrl}", branch: "${config.branch}", credentialsId: "${config.credentialsId}"
-          // git url: '${pipelineParams.url}', branch: '${pipelineParams.branch}'
-           //git branch: '${pipelineParams.branch}', credentialsId: 'Jenkins-git-cred-new', url: '${pipelineParams.url}'
-            //git branch: '${config.branch}', credentialsId: 'Jenkins-git-cred-new', url: '${config.url}'
+            }
         }
-    }
     stage('Build the code'){
         steps{
             sh 'mvn clean install'
