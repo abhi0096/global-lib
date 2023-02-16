f5df9687b9f450c92b6c1b982d63f4a8752f77ee
Abhishek Khandelwal <106994013+abhi0096@users.noreply.github.com>
2023-02-16 11:24:46 +0530

    Update genericFile.groovy

--- vars/genericFile.groovy
+++ vars/genericFile.groovy
@@ -25,9 +25,40 @@ stages {
     }
 }
 }
-//      def pipelineSteps = [Step, buildStep]
-//      return pipelineSteps
 }
+}
+
+// ==================================================================================================================================================
+// #!/usr/bin/env groovy
+
+// def call(Map config) {
+
+// pipeline {
+//      agent any
+//     //  options {
+//     //     copyArtifactPermission('rollback');
+//     // }
+
+// stages {
+//     stage('git-clone') {
+//        steps {
+//             //def step = {
+//               git url: "${config.repoUrl}", branch: "${config.branch}", credentialsId: "${config.credentialsId}"
+//           // git url: '${pipelineParams.url}', branch: '${pipelineParams.branch}'
+//            //git branch: '${pipelineParams.branch}', credentialsId: 'Jenkins-git-cred-new', url: '${pipelineParams.url}'
+//             //git branch: '${config.branch}', credentialsId: 'Jenkins-git-cred-new', url: '${config.url}'
+//         }
+//     }
+//     stage('Build the code'){
+//         steps{
+//             sh 'mvn clean install'
+//         }
+//     }
+// }
+// }
+// //      def pipelineSteps = [Step, buildStep]
+// //      return pipelineSteps
+// }
 
 // #!/usr/bin/env groovy
 
